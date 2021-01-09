783f8f6f968517f11289110e024c7bd9ffa6f1eb
Nayan <nsavaliya4295@gmail.com>
2021-01-09 10:33:01 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,139 @@
+const express = require('express');
+
+const jwt = require('jsonwebtoken');
+
+const cors = require('cors')
+
+const sql = require('./sqlConn');
+
+const app = express();
+
+app.use(express.json());
+
+app.use(cors())
+
+
+app.get('/list', (req, res) => {
+    sql.list().then(data =>res.json(data) );  
+});
+
+
+function verifyToken(req,res,next){
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== "undefined"){
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+        next();
+    }
+    else{
+        res.sendStatus(403);
+    }
+}
+
+
+app.post('/login', (req, res) => {
+    if(!req.body.username || !req.body.password){
+        res.sendStatus(400);
+    }
+    else{
+        sql.authUser(req.body.username,req.body.password).then(
+            (result)=>{
+                if (result === 'true'){
+                    jwt.sign(req.body,'secretKey',{expiresIn:'1h'},
+                    (err, token)=>{
+                        
+                        res.json({"token":token})
+                    });   
+                }
+                else{
+                    res.json({"status":"Invalid Credentials"})
+                }
+
+            }
+        )
+    }
+    
+});
+
+
+
+app.post('/battleApi', verifyToken, (req, res) => {
+    jwt.verify(req.token,'secretKey',(err, data)=>{
+        console.log(data,err);
+        if(err){
+            
+            res.json({err})
+           
+        }
+        else{
+            if(!req.body.battleName ){
+                res.sendStatus(400);
+            }
+            else{
+                sql.createBattle(req.body.battleName).then(
+                    (result)=>{
+                            res.json({result,data})
+                        
+                    }
+                )
+            }
+        }
+    })
+    
+    
+});
+
+app.get('/battleApi', (req, res) => {
+    if(!req.body.battleName ){
+        res.sendStatus(400);
+    }
+    else{
+        sql.readBattle(req.body.battleName).then(
+            (result)=>{
+                    res.json({result})
+                
+
+            }
+        )
+    }
+    
+});
+
+app.delete('/battleApi', (req, res) => {
+    if(!req.body.battleName ){
+        res.sendStatus(400);
+    }
+    else{
+        sql.deleteBattle(req.body.battleName).then(
+            (result)=>{
+                    res.json({result})
+                
+
+            }
+        )
+    }
+    
+});
+
+app.put('/battleApi', (req, res) => {
+    if(!req.body.battleName ||!req.body.newBattleName ){
+        res.sendStatus(400);
+    }
+    else{
+        sql.deleteBattle(req.body.battleName,req.body.newBattleName).then(
+            (result)=>{
+                    res.json({result})
+                
+
+            }
+        )
+    }
+    
+});
+
+
+
+
+
+app.listen(3000, () => console.log("Listening port 3000…"));
---
+++ sqlConn.js
@@ -0,0 +1,155 @@
+
+const mysql = require('mysql');
+
+const conn = mysql.createConnection({
+    host: "localhost",
+    user: "rootnode",
+    password: "root",
+    database: "battles",
+  });
+
+
+function authUser(username,password) {
+
+    return new Promise((resolve, reject)=>{
+        conn.query("SELECT * FROM users", function (err, result) {
+            if (err) reject(err);
+            let user = result[0].username;
+            let pass = result[0].password;
+            user === username && pass === password?resolve("true"):resolve("false");
+            
+        })
+
+    })
+
+}
+
+function readBattle (battleName) {
+
+    return new Promise((resolve, reject)=>{
+        conn.query(`SELECT name FROM battLes_data WHERE name = '${battleName}'`, function (err, result) {
+            if (err) reject(err);
+            let data = result.map((item) => item.name);
+            resolve(data);
+            
+        })
+
+    })
+
+}
+
+function createBattle(battleName) {
+
+    return new Promise((resolve, reject)=>{
+        conn.query(`SELECT name FROM battLes_data WHERE name = '${battleName}'`, function (err, result) {
+            if (err) reject(err);
+            if (result.length === 0){
+                conn.query(`INSERT INTO battLes_data (name) VALUES ('${battleName}')`, function (err, result) {
+                    if (err) reject(err);
+                    conn.query(`SELECT name FROM battLes_data WHERE name = '${battleName}'`, function (err, result) {
+                        if (err) reject(err);
+                        battleName === result[0].name? resolve("Battle Inserted Successfully"):resolve("Please try again");
+                        
+                    })
+                })
+            }
+            else{
+                resolve("Battle already exist");
+                
+            } 
+        })
+
+    })
+
+}
+
+function deleteBattle(battleName) {
+
+    return new Promise((resolve, reject)=>{
+        conn.query(`SELECT name FROM battLes_data WHERE name = '${battleName}'`, function (err, result) {
+            if (err) reject(err);
+            if (result.length===0){
+                resolve("Battle not found");
+             
+            }
+            else if (result[0].name === battleName){
+                conn.query(`DELETE FROM battLes_data WHERE name = '${battleName}'`, function (err, result) {
+                    if (err) reject(err);
+                    conn.query(`SELECT name FROM battLes_data WHERE name = '${battleName}'`, function (err, result) {
+                        if (err) reject(err);
+                        result.length === 0? resolve("Deleted Successfully"):resolve("Please try again");
+                        
+                    })
+                })
+            }
+            else {
+                resolve("Battle not found");
+                
+            }
+        })
+
+    })
+
+}
+
+function updateBattle(battleName, newBattleName) {
+
+    return new Promise((resolve, reject)=>{
+        conn.query(`SELECT name FROM battLes_data WHERE name = '${battleName}'`, function (err, result) {
+            if (err) reject(err);
+            if (result.length===0){
+                resolve("Battle not found");
+                
+            }
+            else if (result[0].name === battleName){
+                conn.query(`UPDATE battles_data SET name = '${newBattleName}' WHERE name = '${battleName}'`, function (err, result) {
+                    if (err) reject(err);
+                    conn.query(`SELECT name FROM battLes_data WHERE name = '${newBattleName}'`, function (err, result) {
+                        if (err) reject(err);
+                        newBattleName === result[0].name? resolve("Updated Successfully"):resolve("Please try again");
+                        
+                    })
+                })
+            }
+            else{
+             resolve("Battle not found");
+             
+            }
+        })
+
+    })
+
+}
+
+function api(){
+
+    deleteBattle("Nayan").then(e => console.log(e))
+
+}
+
+function getList () {
+
+    return new Promise((resolve, reject)=>{
+        conn.query("SELECT DISTINCT region FROM battles_data", function (err, result) {
+            if (err) reject(err);
+            var data = result.map((item) => item.region);
+            resolve(data);
+            
+        })
+        
+
+    })
+    
+
+}
+
+module.exports.list = getList;
+module.exports.authUser = authUser;
+module.exports.createBattle = createBattle;
+module.exports.readBattle = readBattle;
+module.exports.updateBattle = updateBattle;
+module.exports.deleteBattle = deleteBattle;
+
+
+
+
